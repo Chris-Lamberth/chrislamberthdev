6e99eda91581bd25ded8e4bf1c8bb777d97974a8
Chris Lamberth <cmattic@gmail.com>
2023-07-20 17:12:32 -0400

    Instagram Feed backend

---
+++ backend/schemas/instaFeed.js
@@ -0,0 +1,32 @@
+import { defineField, defineType } from 'sanity';
+
+export default defineType({
+  name: 'instaFeed',
+  title: 'Instagram Feed',
+  type: 'document',
+  fields: [
+	defineField({
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    }),
+    defineField({
+      name: 'postURLs',
+      title: 'Post URL',
+      type: 'url',
+    }),
+	 defineField({
+		name: 'postImage',
+		title: 'Image',
+		type: 'image',
+		options: {
+		  hotspot: true,
+		},
+	 }),
+  ],
+  preview: {
+    select: {
+      title: 'name',
+    },
+  },
+});
