3541b5caf0d035bd1bdd65a34f5dbc26f7a4a0c1
Chris Lamberth <cmattic@gmail.com>
2023-07-31 17:04:11 -0400

    Moved ids and tokens into env

--- frontend/src/lib/components/Contact.svelte
+++ frontend/src/lib/components/Contact.svelte
@@ -1,11 +1,7 @@
 <div class="card">
-	<h2 class="section-headline">
-		reach out
-	</h2>
-	<p>
-		Got a rad project or just want to say hello? Shoot me a message and let's chat.
-	</p>
-	<form action="">
+	<h2 class="section-headline">reach out</h2>
+	<p>Got a rad project or just want to say hello? Shoot me a message and let's chat.</p>
+	<form method="POST" use:enhance>
 		<div class="field">
 			<input type="text" id="name" name="name" placeholder="name" />
 		</div>
@@ -13,79 +9,82 @@
 			<input type="email" id="email" placeholder="email" name="email" />
 		</div>
 		<div class="field">
-			<textarea name="message" id="message" placeholder="message" cols="30" rows="5"></textarea>
+			<textarea name="message" id="message" placeholder="message" cols="30" rows="5" />
 		</div>
 		<button type="submit">send</button>
 	</form>
 </div>
 
 <style>
-	.card{
+	.card {
 		background: linear-gradient(100deg, #02242a 0%, #000 50%, #00221d 90%);
 		border-radius: var(--radius);
-		padding:3rem;
+		padding: 3rem;
 	}
-	.card h2{
-		color:#fff;
-		margin:0 0 calc(var(--headline-pad) / 2) 0;
+	.card h2 {
+		color: #fff;
+		margin: 0 0 calc(var(--headline-pad) / 2) 0;
 	}
-	.card p{
-		color:#fff;
-		margin:0 0 var(--headline-pad) 0;
+	.card p {
+		color: #fff;
+		margin: 0 0 var(--headline-pad) 0;
 	}
-	form{
-		display:flex;
+	form {
+		display: flex;
 		flex-direction: column;
-		gap:1rem;
+		gap: 1rem;
 	}
-	input, textarea{
-		width:100%;
-		border:none;
-		padding:.7rem;
-		border-radius:.5em;
-		color:#fff;
-		background: linear-gradient( 90deg, #003037 0%, #003f33 100%);
+	input,
+	textarea {
+		width: 100%;
+		border: none;
+		padding: 0.7rem;
+		border-radius: 0.5em;
+		color: #fff;
+		background: linear-gradient(90deg, #003037 0%, #003f33 100%);
 		box-shadow: 0 0 0 0px #000, 0 0 0 0px #00ffcc;
-		transition:box-shadow .1s ease-in-out;
+		transition: box-shadow 0.1s ease-in-out;
 	}
-	input::placeholder, textarea::placeholder{
-		color:#8bb1ae;
+	input::placeholder,
+	textarea::placeholder {
+		color: #8bb1ae;
 	}
-	input:focus, textarea:focus {
+	input:focus,
+	textarea:focus {
 		outline: none;
 		box-shadow: 0 0 0 2px #000, 0 0 0 4px #00ffcc;
-		
 	}
-	
-	
-	input::-webkit-input-placeholder, form button::webkit-input-placeholder {
-	border: none;
+
+	input::-webkit-input-placeholder,
+	form button::webkit-input-placeholder {
+		border: none;
 	}
-	textarea::-webkit-input-placeholder, form button::webkit-input-placeholder {
-	border: none;
+	textarea::-webkit-input-placeholder,
+	form button::webkit-input-placeholder {
+		border: none;
 	}
-	form button{
-		font-family:var(--serif);
-		letter-spacing: .03em;
-		font-size:1rem;
-		background: linear-gradient( 90deg, #00deff 0%, #00ffcc 100%);
-		padding:1em;
-		border-radius:1000px;
-		min-width:120px;
+	form button {
+		font-family: var(--serif);
+		letter-spacing: 0.03em;
+		font-size: 1rem;
+		background: linear-gradient(90deg, #00deff 0%, #00ffcc 100%);
+		padding: 1em;
+		border-radius: 1000px;
+		min-width: 120px;
 		align-self: flex-end;
 		box-shadow: 0 0 0 0px #000, 0 0 0 0px #00ffcc;
-		transition: scale .15s ease-in-out, box-shadow .1s ease-in-out;
+		transition: scale 0.15s ease-in-out, box-shadow 0.1s ease-in-out;
 	}
-	form button:focus-visible{
+	form button:focus-visible {
 		box-shadow: 0 0 0 2px #000, 0 0 0 4px #00ffcc;
 	}
-	form button:focus{
-		outline:none;
+	form button:focus {
+		outline: none;
 	}
-	form button:hover{
-		scale:1.07;
+	form button:hover {
+		scale: 1.07;
 	}
-	form button:active{
-		scale:.95;
+	form button:active {
+		scale: 0.95;
 	}
 </style>
--- frontend/src/lib/sanity.js
+++ frontend/src/lib/sanity.js
@@ -1,13 +1,14 @@
 import { createClient } from '@sanity/client'; 
 import createImageUrlBuilder from '@sanity/image-url';
+import { PUBLIC_SANITY_PROJECT_ID, PUBLIC_SANITY_TOKEN} from '$env/static/public';
 
 export const config = {  
- projectId: 'ps0x3kbb',  
- dataset: 'production',  
- apiVersion: 'v2023-06-06',  
- token:'skwIZiArC4ryImsMMR0eFueBbjVFqxF6etbgpXRxmwclfoXEuwujHRvCzidXx6FoLaksD2Nh3gljsOSr5QWA9rm3rfunC5fy0LQTDPk6kHVzJOVkyLkxwco3rD2sWgZzJbSnhnM1r9ExwFA8JfPbGQJwpOILaN5EeAqCD9sDBDOQcLXfj7kU',  
- useCdn: true,  
- ignoreBrowserTokenWarning: true  
+	projectId: PUBLIC_SANITY_PROJECT_ID,
+	dataset: 'production',  
+	apiVersion: 'v2023-06-06',  
+	token: PUBLIC_SANITY_TOKEN,
+	useCdn: true,  
+	ignoreBrowserTokenWarning: true  
 };  
 
 export const client = createClient(config); 
