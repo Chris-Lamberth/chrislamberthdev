a8d6a082daaebb693d66e5c9903bb9158667990f
Chris Lamberth <cmattic@gmail.com>
2023-08-15 16:36:14 -0400

    Hooked up the form with Formspree

--- frontend/src/lib/components/Contact.svelte
+++ frontend/src/lib/components/Contact.svelte
@@ -2,22 +2,44 @@
 	let name = '';
 	let email = '';
 	let message = '';
+	let alertMessage = '';
+	let alertMessageType= '';
 
 	const submitForm = async (event) => {
-		 event.preventDefault();
-		 const formData = { name, email, message };
-		 console.log(formData);
+		event.preventDefault(); 
+		const formData = new FormData(event.target);
 
-		 // Here you can handle your form data. 
-		 // For example, make a POST request to your server or use fetch to send this data to some API
+		try {
+			const response = await fetch("https://formspree.io/f/myyqeova", {
+				method: 'POST',
+				body: formData,
+				headers: {
+					'Accept': 'application/json'
+				}
+			});
+
+			if (response.ok) {
+				alertMessage = 'Message Sent!';
+				alertMessageType= 'success';
+				name = '';
+				email = '';
+				message = '';
+			} else {
+				const data = await response.json();
+				alertMessage = data.error;
+				alertMessageType= 'error';
+			}
+		} catch (error) {
+			alertMessage = 'There was an error submitting the form.';
+			alertMessageType= 'error';
+		}
 	};
 </script>
-<h2>name:</h2><p>{name}</p>
-<h2>email:</h2><p>{email}</p>
-<h2>message:</h2><p>{message}</p>
+
 <div class="card">
   <h2 class="section-headline">reach out</h2>
   <p>Got a rad project or just want to say hello? Shoot me a message and let's chat.</p>
+  
   <form on:submit={submitForm}>
 	  <div class="field">
 		  <input type="text" bind:value={name} id="name" name="name" placeholder="name" />
@@ -28,9 +50,14 @@
 	  <div class="field">
 		  <textarea bind:value={message} name="message" id="message" placeholder="message" cols="30" rows="5"></textarea>
 	  </div>
-	  <button type="submit">send</button>
+	 <div class="bottom">
+		<p class="msg {alertMessageType}">{alertMessage}</p>
+		<button type="submit">send</button>
+	 </div>
   </form>
 </div>
+
+
 
 
 <style>
@@ -63,6 +90,22 @@
 		box-shadow: 0 0 0 0px #000, 0 0 0 0px #00ffcc;
 		transition: box-shadow 0.1s ease-in-out;
 	}
+	.bottom{
+		display:flex;
+		justify-content:space-between;
+		align-items: flex-start;
+		gap:1rem;
+	}
+	.msg{
+		margin:0;
+		font-size: .9rem;
+	}
+	.card p.error{
+		color:rgb(255, 98, 0);
+	}
+	.card p.success{
+		color:#00ffcc;
+	}
 	input::placeholder,
 	textarea::placeholder {
 		color: #8bb1ae;
@@ -89,7 +132,6 @@
 		padding: 1em;
 		border-radius: 1000px;
 		min-width: 120px;
-		align-self: flex-end;
 		box-shadow: 0 0 0 0px #000, 0 0 0 0px #00ffcc;
 		transition: scale 0.15s ease-in-out, box-shadow 0.1s ease-in-out;
 	}
--- frontend/src/routes/work/+page.svelte
+++ frontend/src/routes/work/+page.svelte
@@ -1,6 +1,10 @@
 <script>
 	import Feed from '$lib/components/Feed.svelte';
+export let data;
+	const { posts } = data.posts;
 </script>
 
-<Feed />
+<section class="container">
+	<Feed {posts} />
 
+</section>
